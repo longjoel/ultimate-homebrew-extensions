eda6395020694615493a76905bea0e6173bd8ccf
Joel Longanecker <joel.longanecker@gmail.com>
2024-06-26 16:20:25 -0500

    nostalgist FTW

--- src/GBAEmulatorProvider.ts
+++ src/GBAEmulatorProvider.ts
@@ -11,48 +11,28 @@ export class GBAEmulatorProvider implements vscode.CustomReadonlyEditorProvider 
 		return new GBADocument(uri);
 	}
 	resolveCustomEditor(document: GBADocument, webviewPanel: vscode.WebviewPanel, token: vscode.CancellationToken): void | Thenable<void> {
+		webviewPanel.webview.options = {
+			enableScripts: true,
+		};
 
-
-	
-		const mgbaJS = webviewPanel.webview.asWebviewUri(vscode.Uri.joinPath(this.context.extensionUri, 'gba',  'mgba.js'));
-		const mgbaWasm = webviewPanel.webview.asWebviewUri(vscode.Uri.joinPath(this.context.extensionUri, 'gba',  'mgba.wasm'));
-		var ctx = this.context;
-		vscode.workspace.fs.readFile(document.uri).then((content) => {
-
-			vscode.workspace.fs.readFile(vscode.Uri.joinPath(ctx.extensionUri, 'gba', 'index.html'))
-				.then((html) => {
-
-					vscode.workspace.fs.readFile(vscode.Uri.joinPath(ctx.extensionUri, 'gba','mgba.js')).then((mgbaJS) => {
-
-let adjustedBinaryFile = mgbaJS.toString().replace(`wasmBinaryFile="mgba.wasm"`,`wasmBinaryFile="${mgbaWasm}"`);
-
-					const htmlString = html.toString();
-					const htmlWithData = htmlString.replace('%BASE%', 
-						webviewPanel.webview.asWebviewUri(ctx.extensionUri).toString());
-
-					const htmlWithMgbaJS = htmlWithData.replace('<!--MGBAJS-->', `<script unsafe-inline>${mgbaJS}</script>`);	
-					const htmlWithMeta = htmlWithMgbaJS.replace('<!--SECURITY-->',
-					
-						`<meta
-  http-equiv="Content-Security-Policy"
-  content="default-src  ${webviewPanel.webview.cspSource}; img-src ${webviewPanel.webview.cspSource} https:; script-src ${webviewPanel.webview.cspSource}; style-src ${webviewPanel.webview.cspSource};")/>`);
-
-
-					webviewPanel.webview.options = {
-						enableScripts: true,
-						localResourceRoots: [
-						vscode.Uri.joinPath(ctx.extensionUri),
-						vscode.Uri.joinPath(ctx.extensionUri, 'gba'),
-						]
-					};
-					webviewPanel.webview.html = htmlWithMeta;
-					webviewPanel.webview.postMessage({ type: 'init', content: content.toString() });
-
-				});
-			});
-
-		});
-
+		webviewPanel.webview.html = `
+<!doctype html>
+<html lang="en">
+  <head>
+  
+    <link rel="icon" type="image/png" href="https://nostalgist.js.org/favicon.png" />
+    <script src="https://unpkg.com/nostalgist@0.9.2/dist/nostalgist.umd.js"></script>
+  </head>
+  <body>
+    <script>
+	(async function(){const nostalgist = await Nostalgist.launch({
+  core: 'mgba',
+  rom: '${webviewPanel.webview.asWebviewUri(document.uri)}',
+	})})();
+	</script>
+  </body>
+</html>
+		`;
 
 	}
 
