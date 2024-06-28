c56890093a7281d58be6d673d37c81c327f0fa26
Joel Longanecker <joel.longanecker@gmail.com>
2024-06-28 12:03:53 -0500

    simplify some things

--- src/extension.ts
+++ src/extension.ts
@@ -3,7 +3,6 @@
 import * as vscode from 'vscode';
 import { GBAEmulatorProvider } from './GBAEmulatorProvider';
 import { NESEmulatorProvider } from './NESEmulatorProvider';
-import { GameboyEmulatorProvider } from './GameboyEmulatorProvider';
 import { SNESEmulatorProvider } from './SNESEmulatorProvider';
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -14,9 +13,6 @@ export function activate(context: vscode.ExtensionContext) {
 
 	const nesemulator = vscode.window.registerCustomEditorProvider('ultimate-homebrew-extensions.nes-emulator', new NESEmulatorProvider(context));
 	context.subscriptions.push(nesemulator);
-
-	const gbEmulator = vscode.window.registerCustomEditorProvider('ultimate-homebrew-extensions.gb-emulator', new GameboyEmulatorProvider(context));
-	context.subscriptions.push(gbEmulator);
 
 	const snesEmulator = vscode.window.registerCustomEditorProvider('ultimate-homebrew-extensions.snes-emulator', new SNESEmulatorProvider(context));
 	context.subscriptions.push(snesEmulator);
