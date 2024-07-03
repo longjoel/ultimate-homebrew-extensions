7acb241c8b81817d149d1aaaede6b43f043bfcc8
PeterJacobson337 <peter.jacobson337@gmail.com>
2024-07-03 16:52:10 -0500

    Webviews retain content when focus is lost

--- src/extension.ts
+++ src/extension.ts
@@ -1,22 +1,41 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { GBAEmulatorProvider } from './GBAEmulatorProvider';
 import { NESEmulatorProvider } from './NESEmulatorProvider';
 import { SNESEmulatorProvider } from './SNESEmulatorProvider';
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+
 export function activate(context: vscode.ExtensionContext) {
-	
-	const gbaEmulator = vscode.window.registerCustomEditorProvider('ultimate-homebrew-extensions.gba-emulator', new GBAEmulatorProvider(context));
-	context.subscriptions.push(gbaEmulator);
+    const gbaEmulator = vscode.window.registerCustomEditorProvider(
+        'ultimate-homebrew-extensions.gba-emulator',
+        new GBAEmulatorProvider(context),
+        {
+            webviewOptions: {
+                retainContextWhenHidden: true
+            }
+        }
+    );
+    context.subscriptions.push(gbaEmulator);
 
-	const nesemulator = vscode.window.registerCustomEditorProvider('ultimate-homebrew-extensions.nes-emulator', new NESEmulatorProvider(context));
-	context.subscriptions.push(nesemulator);
+    const nesEmulator = vscode.window.registerCustomEditorProvider(
+        'ultimate-homebrew-extensions.nes-emulator',
+        new NESEmulatorProvider(context),
+        {
+            webviewOptions: {
+                retainContextWhenHidden: true
+            }
+        }
+    );
+    context.subscriptions.push(nesEmulator);
 
-	const snesEmulator = vscode.window.registerCustomEditorProvider('ultimate-homebrew-extensions.snes-emulator', new SNESEmulatorProvider(context));
-	context.subscriptions.push(snesEmulator);
+    const snesEmulator = vscode.window.registerCustomEditorProvider(
+        'ultimate-homebrew-extensions.snes-emulator',
+        new SNESEmulatorProvider(context),
+        {
+            webviewOptions: {
+                retainContextWhenHidden: true
+            }
+        }
+    );
+    context.subscriptions.push(snesEmulator);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
